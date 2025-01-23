39a8f14d64a1991953571eb7ef2f3ad3382a4b1e
kelsey adams <kelseyjaneadams3@gmail.com>
2025-01-23 18:21:31 +0000

    move SECRET_KEY to environment variable for improved security

--- chaatwala_project/settings.py
+++ chaatwala_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o)uir*(bg0jt__e&wtu6!%so_lo^xx)4nfc2_gq9zhrvzk=492'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
