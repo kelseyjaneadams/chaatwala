fb558756c4f846bb03f5e43d09fc402f90558f18
kelsey adams <kelseyjaneadams3@gmail.com>
2025-01-24 14:46:20 +0000

    Create reviews app and configure project-level URLs

--- chaatwala_project/urls.py
+++ chaatwala_project/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from bookings.views import my_bookings
+from django.urls import path, include
 
 urlpatterns = [
-    path('bookings/', my_bookings, name='bookings'),
+    path('reviews/', include('reviews.urls')),
+    path('bookings/', include('bookings.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ reviews/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.reviews_home, name='reviews_home'), 
+]
