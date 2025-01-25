c97c5ce07dc2b7de7a87baa2bb749f251c782b4f
kelsey adams <kelseyjaneadams3@gmail.com>
2025-01-25 12:47:54 +0000

    Define code for Profiles model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -17,6 +18,7 @@ class Review(models.Model):
         User,
         on_delete=models.CASCADE
     )
+    # add max character length?
     rating = models.IntegerField() 
     comment = models.TextField(
         blank=True,
@@ -25,6 +27,17 @@ class Review(models.Model):
     review_date = models.DateTimeField(
         auto_now_add=True
     )
+
+
+    def clean(self):
+        """
+        Validates the comment field to ensure it does not exceed
+        the maximum allowed character length of 1000.
+        """
+        super().clean()
+        if self.comment and len(self.comment) > 500: 
+            raise ValidationError("Comment cannot exceed 1000 characters.")
+    
 
     def __str__(self):
         return f"Review {self.review_id} by {self.user.username}"
