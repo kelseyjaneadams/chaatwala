febefddc986958b7e46a716581ad25a56dca7ef5
kelsey adams <kelseyjaneadams3@gmail.com>
2025-01-25 17:49:47 +0000

    Set up admin configurations for Profile model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Admin interface for managing user profiles.
+    Displays user and profile image in the admin panel.
+    """
+    list_display = ('user', 'image')
+    search_fields = ('user__username',)
